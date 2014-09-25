8b23d1af78c4581aee29d240ea0bcc5da40c607c
br1cked <br1cked@users.noreply.github.com>
2014-09-25 02:33:53 -1000

    updated tor exit node list URL
    The old tor exit node list URL only listed exit nodes that could reach a specific ip (1.1.1.1) and port (80). This URL lists all exit nodes.

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -5,7 +5,7 @@ IP_BLACKLIST_TMP=/tmp/ip-blacklist.tmp
 IP_BLACKLIST_CUSTOM=/etc/ip-blacklist-custom.conf # optional
 BLACKLISTS=(
 "http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1" # Project Honey Pot Directory of Dictionary Attacker IPs
-"http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"  # TOR Exit Nodes
+"https://check.torproject.org/exit-addresses"  # TOR Exit Nodes (requires wget with HTTPS/SSL support)
 "http://www.maxmind.com/en/anonymous_proxies" # MaxMind GeoIP Anonymous Proxies
 "http://danger.rulez.sk/projects/bruteforceblocker/blist.php" # BruteForceBlocker IP List
 "http://rules.emergingthreats.net/blockrules/rbn-ips.txt" # Emerging Threats - Russian Business Networks List
